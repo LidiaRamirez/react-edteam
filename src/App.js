bb8cf7dbb7b53b6be1c8a5c2e382ddbdec403ea0
Lidia <lidia.ramirezn@gmail.com>
2019-10-02 15:28:37 -0500

    arrays

--- src/App.js
+++ src/App.js
@@ -2,6 +2,37 @@ import React from 'react';
 import Curso from './Curso'
 import './styles/styles.scss';
 
+const cursos = [
+  {
+    indice: 'c1',
+    title: "React desde cero",
+    image: "https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-04/React%20desde%20cero%20%281%29.png",
+    price: 20,
+    teacher: "Alexys Lozada"
+  }, 
+  {
+    indice: 'c2',
+    title: "Angular Desde Cero",
+    image: "https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-08/angular.png",
+    price: 20,
+    teacher: "Luis Aviles"
+  }, 
+  {
+    indice: 'c3',
+    title: "Firebase",
+    image: "https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-07/firebase-web.png",
+    price: 20,
+    teacher: "Juan Guillermo Gomez"
+  }, 
+  {
+    indice: 'c4',
+    title: "Adobe Xd desde cero",
+    image: "https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-09/adobe-xd.png",
+    price: 20,
+    teacher: "Alvaro Felipe"
+  }
+]
+
 const App = () => (
   <>
   <div className="main-banner img-container l-section" id="main-banner">
@@ -17,15 +48,25 @@ const App = () => (
     </div>
   </div>
   <div className="ed-grid m-grid-3 ">
-    <Curso 
+    {/* <Curso 
       title="React desde cero" 
       image="https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-04/React%20desde%20cero%20%281%29.png"
       price= {20}
       profesor="Alexys Lozada"
-    />
-      
-    <Curso 
-    />
+    /> 
+    <Curso/>
+    */}
+
+    {
+      cursos.map( (curso, index) =>        
+        <Curso key={index}
+          title= {curso.title}
+          image = {curso.image}
+          price = {curso.price}
+          profesor = {curso.teacher}
+        />)
+    }
+    
   </div>
   
 </>
