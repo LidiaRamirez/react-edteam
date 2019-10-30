05a826eed57155e5289356d65fda218c583760a9
Lidia <lidia.ramirezn@gmail.com>
2019-10-30 10:49:04 -0500

    Modificando README y agregando Links

---
+++ src/Historial.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Historial = ( { match, location, history }) => (
+  <div>    
+    <div>{ JSON.stringify(match) }</div>
+    <div>{ JSON.stringify(location) }</div>
+    <div>{ JSON.stringify(history) }</div>
+  </div>
+)
+
+export default Historial
